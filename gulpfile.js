f2aa3324e9c20f4e82ed44dc7e21a247f0012dd8
Salvatore Laisa <moebiusmania@gmail.com>
2015-09-04 10:49:45 +0200

    added first Gulp actions

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,64 @@ Configuration parameters
 ========================
 */
 var conf = {
-	localPort: 3972,
-	cssOutputPath: './dist/css/',
-	scssSourcePath: './app/assets/scss/'
+	scssSourcePath: './app/assets/scss/**/*.{scss,sass}',
+	localPort: 5792,
+	cssOutputPath: './dist/css/'
 }
+
 
 /* 
 Imports
 ========================
 */
 var gulp = require('gulp'),
-	webserver = require('gulp-webserver');
+	webserver = require('gulp-webserver'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps');
+
 
 /* 
 Taks definitions 
 ========================
 */
+
+// Create a webserver and open browser on location
 gulp.task('webserver', function() {
   gulp.src('./')
     .pipe(webserver({
-      directoryListing: true,
-      host: '0.0.0.0',
-      open: true,
-      port: conf.localPort
+      directoryListing:{
+		    enable:true,
+		    path: 'app/',
+		    fallback: 'index.html'
+		},
+		host: '0.0.0.0',
+		fallback: './app/index.html',
+		open: true,
+		port: conf.localPort
     }));
 });
+
+// Compiles SASS to CSS
+gulp.task('sass', function () {
+  gulp.src(conf.scssSourcePath)
+  	.pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(conf.cssOutputPath));
+});
+
+// Watch file changes
+gulp.task('watch', function() {
+  gulp.watch(conf.scssSourcePath, ['sass'])
+})
+
 
 /* 
 Actions
 ========================
 */
-gulp.task('default', ['webserver']); 
+
+// Launch a webserver and watch for *.scss, *.js, *.jsx changes and recompiles in plain .css or .js in the /dist folder
+gulp.task('default', ['webserver', 'watch']); 
