f60c53337e67fab1cadd05eb93aa0c41e4d07142
Salvatore Laisa <moebiusmania@gmail.com>
2015-09-25 10:02:17 +0200

    more components

---
+++ app/components/progress-bar/script.js
@@ -0,0 +1,54 @@
+Polymer({
+	is: 'progress-bar',
+	properties:{
+		value: {
+			type: Number,
+			value: 0
+		},
+		minimal: {
+			type: Boolean,
+			value: false
+		},
+		primary: {
+			type: Boolean,
+			value: false
+		},
+		secondary: {
+			type: Boolean,
+			value: false
+		},
+		info: {
+			type: Boolean,
+			value: false
+		},
+		success: {
+			type: Boolean,
+			value: false
+		},
+		warning: {
+			type: Boolean,
+			value: false
+		},
+		error: {
+			type: Boolean,
+			value: false
+		}
+	},
+	attached: function(){
+		this.$.label.classList.remove('progress-bar');
+	},
+	computeProp: function(value){
+		return 'width:' + this.value + '%;';
+	},
+	computeClass: function(minimal,color){
+		var vals = ['minimal','primary','secondary','info','success','warning','error'];
+		var str = '';
+		for(var i = 0; i < arguments.length; i++){
+			arguments[i] ? str += (vals[i] + ' ') : str;
+		}
+		return str;
+	},
+	percent: function(value){
+		return value + '%'
+	}
+});
